5a5bda31675b18fa3857a88bf4f453532260160d
Arham~ <53353729+syedarhamali@users.noreply.github.com>
2022-07-01 00:59:21 +0500

    Update care-webapp.sql

--- care-webapp.sql
+++ care-webapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 17, 2021 at 06:46 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Host: localhost
+-- Generation Time: Jun 30, 2022 at 09:58 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,13 +36,6 @@ CREATE TABLE `admin_tbl` (
   `picture` varchar(300) NOT NULL,
   `role` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `admin_tbl`
---
-
-INSERT INTO `admin_tbl` (`id`, `name`, `email`, `cell`, `password`, `picture`, `role`) VALUES
-(21, 'arham', 'arham@gmail.com', '03002303974', 'fcea920f7412b5da7be0cf42b8c93759', 'images2.jpg', 'Admin');
 
 -- --------------------------------------------------------
 
@@ -101,13 +94,6 @@ CREATE TABLE `doctor_tbl` (
   `status` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `doctor_tbl`
---
-
-INSERT INTO `doctor_tbl` (`id`, `name`, `email`, `password`, `pic`, `role`, `dr_address`, `apt_date`, `apt_time`, `dr_about`, `speciality`, `degree_name`, `speciality_2`, `speciality_3`, `cell`, `gender`, `edu_bg`, `avail_days`, `avail_time`, `status`) VALUES
-(18, 'arham', 'arham@gmail.com', '1f32aa4c9a1d2ea010adcf2348166a04', 'imagess.jpg', '', 'C-224 Aleemabad', '', '', '', 'Neuro', 'MSc.', 'Sycology', 'Heart Disease', '03002303974', 'Male', 'Intermediate', 'Monday to Friday', 12, 'approved');
-
 -- --------------------------------------------------------
 
 --
@@ -140,15 +126,6 @@ CREATE TABLE `user_tbl` (
   `apointment` varchar(300) NOT NULL,
   `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_tbl`
---
-
-INSERT INTO `user_tbl` (`id`, `name`, `email`, `password`, `cell`, `age`, `apointment`, `status`) VALUES
-(11, 'arham', 'arhamali@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '03002303974', 23, '', 'approved'),
-(12, 'arham', 'umar@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '03002303974', 23, '', 'approved'),
-(13, 'umar', 'saif@gmail.com', '202cb962ac59075b964b07152d234b70', '03002303974', 23, '', 'approved');
 
 --
 -- Indexes for dumped tables
@@ -192,7 +169,7 @@ ALTER TABLE `user_tbl`
 -- AUTO_INCREMENT for table `admin_tbl`
 --
 ALTER TABLE `admin_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `doctor_pending`
@@ -210,13 +187,23 @@ ALTER TABLE `doctor_tbl`
 -- AUTO_INCREMENT for table `pending_users`
 --
 ALTER TABLE `pending_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  ADD CONSTRAINT `user_tbl_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pending_users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
